806d2fca3602f9fdd0f5ba3f6c295738e4c88e83
ojas032 <corpojasltd@gmial.com>
2020-09-16 18:39:32 +0530

    added keystroke functionality

---
+++ app.js
@@ -0,0 +1,24 @@
+var budgetController=(function(){
+    
+    
+    //some code
+
+
+})();
+
+var UIcontroller=(function(){
+    
+    
+    //somecode
+
+
+})();
+
+
+var controller =(function(){
+
+    document.querySelector('.add__btn').addEventListener('click',function(event){
+
+        console.log(event);
+    })
+})();
